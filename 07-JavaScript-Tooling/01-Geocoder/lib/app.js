46d204acb6812cbcdeb4065dad6873c549346b38
(Amanda Orbe) <garayomiriam@gmail.com>
2018-04-18 19:30:39 +0200

    geocoding

--- 07-JavaScript-Tooling/01-Geocoder/lib/app.js
+++ 07-JavaScript-Tooling/01-Geocoder/lib/app.js
@@ -1,1 +1,48 @@
 // TODO: Put your JS code in here
+import GMaps from 'gmaps';
+const latitude = (data) => {
+  return data.results[0].geometry.location.lat;
+}
+
+const longitude = (data) => {
+  return data.results[0].geometry.location.lng;
+}
+
+
+
+const logData = (data) => {
+
+  const form = document.querySelector("form");
+  form.insertAdjacentHTML("beforeend", `<p>latitude = ${latitude(data)} longitude = ${longitude(data)}</p>`)
+
+};
+const logMap = (data) => {
+  const map = new GMaps({ el: '#map', lat: latitude(data), lng: longitude(data), zoom: 14 });
+  const marker = { lat: latitude(data), lng:longitude(data)};
+  map.addMarkers([ marker ]);
+}
+
+
+const autocomplete = (e) => {
+  event.preventDefault();
+  const address = document.getElementById("search").value
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
+  console.log(url)
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      logData(data);
+      logMap(data);
+
+    });
+
+};
+
+
+
+
+const input = document.getElementById("geocode"); // that's an <input id="search">
+
+
+
+input.addEventListener("click", autocomplete);
